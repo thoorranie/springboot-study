c355a44f4d8c7a4a612c062ba610c90f9c350b47
thoorranie <thoorranie1@gmail.com>
2026-07-16 15:29:52 +0900

    62~94p

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Member(String name){
+        this.name = name;
+    }
+}
